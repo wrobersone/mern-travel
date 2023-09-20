04d16e2aa65572ddfc35db23f2b38c5075c1c6f1
William Robersone <william.robersone@gmail.com>
2023-09-19 21:11:26 -0500

    fifth commit

--- frontend/src/pages/Locations.jsx
+++ frontend/src/pages/Locations.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Common from "../shared/Common"
+
 import LocationCard from "../components/Cards/LocationCard"
 import Newsletter from '../shared/Newsletter'
 import locationData from '../data/locations'
@@ -7,6 +8,14 @@ import { Col, Container, Row } from "reactstrap"
 import SearchBar from '../shared/SearchBar'
 
 const Locations = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const pages = Math.ceil(5/4);
+    setPageCount(pages);
+  },[page]);
+
   return (
     <>
       <Common title={'Our Vacations'} />
@@ -17,15 +26,25 @@ const Locations = () => {
           </Row>
         </Container>
       </section>
-      <section>
+      <section className="pt-0">
         <Container>
           <Row>
             {locationData.map(
               location => 
-              <Col lg='3' key={location.id}>
+              <Col lg='3' className="mb-4" key={location.id}>
                 <LocationCard location={location} />
               </Col>
             )}
+
+            <Col lg='12'>
+              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                {[...Array(pageCount).keys()].map(number => (
+                  <span key={number} onClick={() => setPage(number)}
+                    className={page === number ? 'active__page' : ''}
+                  >{number + 1}</span>
+                ))}
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
